568e5a08ebe65cb8f706902c69b5e5b1f4a60c29
root <root@DESKTOP-4NET6EQ.localdomain>
2022-12-31 11:14:22 +0400

    finishing core Models

--- hrms/models.py
+++ hrms/models.py
@@ -8,14 +8,14 @@ class Region(models.Model):
 
 class Country(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
-    region = models.ForeignKey(Region, on_delete=models.SET_NULL)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
     
     
 class Location(models.Model):
     street_address = models.TextField(max_length=1000)
     postal_code = models.CharField(max_length=255)
     city = models.CharField(max_length=255, null=False, blank=False)
-    state = models.CharField(max_length=255)
+    state = models.CharField(max_length=255, null=True, blank=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     
     
@@ -32,30 +32,32 @@ class Employee(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{8,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
     hire_date = models.DateField()
-    job = models.ForeignKey(Job, on_delete=models.SET_NULL)
+    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
     salary = models.DecimalField(max_digits=8, decimal_places=2)
-    comission_pct = models.DecimalField(max_digits=4, decimal_places=4)
-    manager = models.ForeignKey('self', on_delete=models.SET_NULL) # change manager when department is changed in signals
-    department = models.ForeignKey('Department', on_delete=models.SET_NULL) # will add on_change in signals
+    comission_pct = models.DecimalField(max_digits=4, decimal_places=4, null=True)
+    manager = models.ForeignKey('self', on_delete=models.SET_NULL, null=True) # change manager when department is changed in signals
+    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True) # will add on_change in signals
     
     
 class Department(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True)
-    manger = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True) # add on_change to department employees in signals
+    manager = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, related_name='dep_manager') # add on_change to department employees in signals
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     
     
 class JobHistory(models.Model):
     # This will be used in employee promotion, transfer or service ending form and will signal employee
+    class Meta:
+        unique_together = (('employee', 'start_date'),)
     employee = models.ForeignKey(Employee, on_delete=models.DO_NOTHING)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True)
     job = models.ForeignKey(Job, on_delete=models.DO_NOTHING)
     department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
     
-    
 class JobGrade(models.Model):
-    level = models.IntegerField(max_length=2, primary_key=True, null=False, unique=True)
-    lowest_sal = models.DecimalField(max_digits=8, decimal_places=2)
-    highest_sal = models.DecimalField(max_digits=8, decimal_places=2)
+    id = models.AutoField(primary_key=True)
+    level = models.CharField(max_length=2, null=False, unique=True)
+    lowest_sal = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    highest_sal = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     
--- hrms/views.py
+++ hrms/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from django.db.models import Count, F
+from .models import Employee
+from django_pandas.io import read_frame
+import pandas as pd
+import plotly.express as px
 
 def hello(request):
-    return HttpResponse('<h1 style="color:red;"> Hello <h1>')
+    qs = Employee.objects.values('department').annotate(
+    num_emp=Count('id'), dep=F('department__name'), office=F('department__location__city'),
+    country=F('department__location__country__name'), region=F('department__location__country__region__name'))
+    df = read_frame(qs)
+    df.drop(columns=['department'], inplace=True)
+    fig = px.sunburst(data_frame=df, values='num_emp', path=['region','country','office', 'dep'])
+    chart = fig.to_html()
+    
+    return HttpResponse(chart)
     
 
 # Create your views here.
